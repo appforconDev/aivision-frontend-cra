5273b40c1d04c181db7e408eba5543493f9998c4
appforconDev <appforcon@gmail.com>
2025-05-16 11:31:16 +0200

    Fix: Lagt till UX med

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,2 @@
-export const backendUrl = "https://sunny-laughter-production-4c2c.up.railway.app/api";
+const backendUrl = "https://sunny-laughter-production-4c2c.up.railway.app/api";
+export default backendUrl;  // Använd default export
