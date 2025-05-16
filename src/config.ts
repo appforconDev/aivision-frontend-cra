57cdc1b50065f68e8f5375f2b0438574268a4532
appforconDev <appforcon@gmail.com>
2025-05-16 11:28:56 +0200

    Fix: Lagt till UX med

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,1 @@
-const raw = process.env.REACT_APP_BACKEND_URL;
-if (!raw) {
-  throw new Error("Missing env var REACT_APP_BACKEND_URL");
-}
-
-// Ensure we always have a string value
-const backendUrl: string = raw.trim();
-
-export default backendUrl;
+export const backendUrl = "https://sunny-laughter-production-4c2c.up.railway.app/api";
