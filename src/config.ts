731d36e91e322d79c71332e5bbc3aed5bd9708d5
appforconDev <appforcon@gmail.com>
2025-05-16 11:21:55 +0200

    Fix: Lagt till UX med

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,9 @@
-// src/config.ts
 const raw = process.env.REACT_APP_BACKEND_URL;
 if (!raw) {
-  throw new Error("Missing env REACT_APP_BACKEND_URL");
+  throw new Error("Missing env var REACT_APP_BACKEND_URL");
 }
-const BACKEND_URL: string = raw;
-export default BACKEND_URL;
+
+// Nu vet TypeScript att BACKEND_URL är en string
+const backendUrl: string = raw;
+
+export default backendUrl;
